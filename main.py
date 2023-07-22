12e82b6610505900f541b2144204ad3f58fa3953
Kevin Zhang <kazp058@gmail.com>
2023-07-21 20:51:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-string = "algoritmo aguacate albacora algoritmica algoritmo 123549 0coco 255algoritmo 55662"
-pattern = "a"
+string = " aaa" #algoritmo aguacate albacora algoritmica algoritmo 123549 0coco 255algoritmo 55662467 
+pattern = "aa"
 
 
 def get_shifts(string: str, pattern:str) -> list:
@@ -7,7 +7,8 @@ def get_shifts(string: str, pattern:str) -> list:
     x_1 = len(pattern)
 
     concurr = []
-    while x_1 < len(string):
+    while x_1 < len(string)+1:
+        segmento = string[x_0:x_1]
         for i in range(len(pattern)):
             i += x_0
             if string[i] == pattern[0]:
@@ -21,11 +22,13 @@ def matches(string:str, pattern:str) -> list:
     match_list = []
     for shift in shifts:
         is_equal = True
-        for i in range(2,len(pattern)):
+        for i in range(1,len(pattern)):
+            if shift + i < len(string):
+                break
             is_equal = is_equal and (pattern[i] == string[shift + i])
         if is_equal:
             match_list.append(shift)
     return match_list
 
-print(get_shifts(string, pattern))
+#print(get_shifts(string, pattern))
 print(matches(string, pattern))
